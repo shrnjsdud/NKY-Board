f45f368aead10c2fe07d7f83ba85ebd6bbb8e5b8
shrnjsdud <84651228+shrnjsdud@users.noreply.github.com>
2023-11-21 21:18:38 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/NKY-Board.jar"
+JAR_FILE="$PROJECT_ROOT/target/NKY-Board-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
-
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
